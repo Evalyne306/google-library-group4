0823e49738875d1438d55790b99e5b7780a4e642
Evalyne306 <evalyne.wambui@student.moringaschool.com>
2023-01-25 16:19:26 +0300

    update

---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import React,{useState} from "react";
+import Card from "./BookCard";
+
+
+export default function Main(){
+    const[search, setSearch]= useState("");
+const[bookData, setData]= useState([]);
+const searchBook=(event)=>{
+    if(event.key==="Enter"){
+
+        axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyCqOKrqzMnQR7vgAcnOFa4WcfUD9djIuX4')
+        .then(res=>setData(res.data.items))
+            .catch(err=>console.log(err))
+        }
+    }
+    return(
+        <>
+        <div className="header">
+            <div className="search">
+                <input type="text" placeholder="Enter your book name" value={search} onChange={e=>setSearch(e.target.value)} onKeyPress={searchBook}/>
+<button>button</button>
+            </div>
+            <img src="" alt=""/>
+        </div>
+        <div className="container">
+          {
+ <Card book={bookData}/>
+          }
+           
+        </div>
+        </>
+    )
+}
