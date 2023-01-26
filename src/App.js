c1c8c2b363c599a9d7d90f74b269b61b583e959c
aminahagi <amina.hagi@student.moringaschool.com>
2023-01-26 14:40:55 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React, { useState } from "react";
 import './App.css';
-import  Login  from './components/Login/Login';
-import  Signup  from './components/Login/Signup';
+import MainLogin from "./components/Login/MainLogin";
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (forName) => {
-    setCurrentForm(forName);
-  }
-
+ 
   return (
     <div className="App">
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Signup onFormSwitch={toggleForm}  />
-      }
+     <MainLogin/>
     </div>
   );
 }
